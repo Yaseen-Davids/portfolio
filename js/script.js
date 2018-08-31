9226bc2c944cd094f304ef571a863e4205c5dd1a
Yaseen Davids <yaseendavids477@gmail.com>
2018-08-31 21:34:30 +0200

    home and skills section added

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    $("#home-section-title").css('transform', 'scale(1)');
+    
+    const skillsPos = $("#skills_section").position().top - 50;
+    const expPos = $("#exp_section").position().top - 50 ;
+
+
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+
+        if (scroll < skillsPos){
+            $("#navbar_links a").removeClass('position');
+        }
+        if (scroll > skillsPos){
+            $("#navbar_links a").removeClass('position');
+            $("#navbar_links a:nth-child(1)").first().addClass('position');
+        }
+        if (scroll > expPos){
+            $("#navbar_links a").removeClass('position');
+            $("#navbar_links a:nth-child(2)").first().addClass('position');
+        }
+    });
+
+    $("#arrow_skills").on('click', function(){
+        $('html,body').animate({scrollTop: skillsPos + 20},1000);
+    })
+
+    $("#navbar_links a").on('click', function(){
+        if ($(this).html() == 'Skills'){
+            $('html,body').animate({scrollTop: skillsPos + 20},1000);
+        }
+    })
+
+})
